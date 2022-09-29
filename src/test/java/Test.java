a0a90d1157d0fbb2b07302660284dbda35f74277
frameworksselenium <frameworksselenium@gmail.com>
2022-09-29 11:08:53 -0400

    initial commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,20 @@
+
+import org.testng.annotations.*;
+
+public class Test {
+
+		
+		@BeforeTest
+		public static void setup() {
+			System.out.println("==================Before Test==================");
+		}	
+		@AfterClass
+		public static void extReport(){
+			System.out.println("==================After Test==================");
+		}
+		@AfterTest
+		private void test(){
+			System.out.println("==================Test==================");
+		}
+
+}
