3fd93c5817289de03b2ec903e35081080dd6530c
frameworksselenium <frameworksselenium@gmail.com>
2022-09-29 11:42:21 -0400

    given spaces

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,14 +7,17 @@ public class Test {
 		@BeforeTest
 		public static void setup() {
 			System.out.println("==================Before Test==================");
+
 		}	
 		@AfterClass
 		public static void extReport(){
 			System.out.println("==================After Test==================");
+
 		}
 		@AfterTest
 		private void test(){
 			System.out.println("==================Test==================");
+
 		}
 
 }
