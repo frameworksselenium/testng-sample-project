4155473b32cf15689f1e58d6976d081e1a586219
frameworksselenium <frameworksselenium@gmail.com>
2022-09-29 12:06:13 -0400

    included asserts

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,5 @@
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class Test {
@@ -7,17 +8,17 @@ public class Test {
 		@BeforeTest
 		public static void setup() {
 			System.out.println("==================Before Test==================");
-
+			Assert.assertEquals("r", "r");
 		}	
 		@AfterClass
 		public static void extReport(){
 			System.out.println("==================After Test==================");
-
+			Assert.assertEquals("r", "r");
 		}
 		@AfterTest
 		private void test(){
 			System.out.println("==================Test==================");
-
+			Assert.assertEquals("r", "tt");
 		}
 
 }
